0b8a2ce6d625ddd25ab5f0e620da1fae8fc91962
omobolaji <abdjinadquadri@gmail.com>
2022-11-30 20:08:19 +0100

    Second solution 'Palindrom Number'

---
+++ src/Easy/palindromeNumber/palindromeNumber.java
@@ -0,0 +1,44 @@
+package Easy.palindromeNumber;
+
+//Problem
+//
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+
+//class palindromeNumber.Solution {
+//    public boolean isPalindrome(int x) {
+//        String pal = Integer.toString(x);
+//        String indrome = "";
+//
+//        for(int y = (pal.length() - 1); y >= 0; y--) {
+//            indrome = indrome.concat(Character.toString(pal.charAt(y)));
+//        }
+//
+//        return pal.equals(indrome);
+//    }
+//}
+
+//Faster code
+class Solution {
+    public boolean isPalindrome(int x) {
+        String pal = Integer.toString(x);
+        StringBuilder indrome = new StringBuilder();
+
+        for(int y = (pal.length() - 1); y >= 0; y--) {
+            indrome.append(pal.charAt(y));
+        }
+
+        return pal.equals(indrome.toString());
+    }
+}
+
+public class palindromeNumber {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        //test cases
+        System.out.println(solution.isPalindrome(121));
+        System.out.println(solution.isPalindrome(-121));
+        System.out.println(solution.isPalindrome(10));
+    }
+}
+
