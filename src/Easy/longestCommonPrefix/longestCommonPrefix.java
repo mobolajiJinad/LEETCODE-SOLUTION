36ec91350843e8bdff0462876dac229112d8eea0
omobolaji <abdjinadquadri@gmail.com>
2022-12-01 15:19:04 +0100

    New leetcode solution 'longest Common Prefix'

---
+++ src/Easy/longestCommonPrefix/longestCommonPrefix.java
@@ -0,0 +1,33 @@
+package Easy.longestCommonPrefix;
+
+import java.util.Arrays;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder output = new StringBuilder();
+
+        Arrays.sort(strs);
+
+        String firstWord = strs[0];
+        String lastWord = strs[strs.length - 1];
+
+        for(int x = 0; x < firstWord.length(); x++) {
+            if(firstWord.charAt(x) == lastWord.charAt(x)) {
+                output.append(lastWord.charAt(x));
+            } else {
+                break;
+            }
+        }
+
+        return output.toString();
+    }
+}
+
+public class longestCommonPrefix {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.longestCommonPrefix(new String[] {"flower","flow","flight"}));
+        System.out.println(solution.longestCommonPrefix(new String[] {"dog","racecar","car"}));
+    }
+}
