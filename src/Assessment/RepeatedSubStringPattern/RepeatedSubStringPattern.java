47f5b16583f6040912f5273bbace32ee6de00e14
omobolaji <abdjinadquadri@gmail.com>
2023-01-02 10:57:41 +0100

    Dont know what im doing again

---
+++ src/Assessment/RepeatedSubStringPattern/RepeatedSubStringPattern.java
@@ -0,0 +1,27 @@
+// Problem
+
+// A peak element is an element that is strictly greater than its neighbors.
+// Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+// You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+// You must write an algorithm that runs in O(log n) time.
+
+// Example 1:
+// Input: s = "abab"
+// Output: true
+// Explanation: It is the substring "ab" twice.
+
+// Example 2:
+// Input: s = "aba"
+// Output: false
+
+// Example 3:
+// Input: s = "abcabcabcabc"
+// Output: true
+// Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+
+
+package Assessment.RepeatedSubStringPattern;
+
+public class RepeatedSubStringPattern {
+    
+}
