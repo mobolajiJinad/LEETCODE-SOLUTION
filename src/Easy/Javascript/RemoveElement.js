c603ebb17db1de5c1a32a23feaf42b3f6714df6e
omobolaji <abdjinadquadri@gmail.com>
2023-03-11 21:45:44 +0100

    New solution

--- src/Easy/Javascript/RemoveElement.js
+++ src/Easy/Javascript/RemoveElement.js
@@ -1,22 +1,25 @@
-//Problem
-// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+// Problem
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
 // Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
 // Return k after placing the final result in the first k slots of nums.
 // Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 
 /**
  * @param {number[]} nums
+ * @param {number} val
  * @return {number}
  */
 
-var removeDuplicates = function (nums) {
-  if (nums.length === 0) return 0;
-  let i = 0;
-  for (let j = 1; j < nums.length; j++) {
-    if (nums[j] !== nums[i]) {
-      i++;
-      nums[i] = nums[j];
+var removeElement = function (nums, val) {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i -= 1;
+    } else {
+      k += 1;
     }
   }
-  return i + 1;
+
+  return k;
 };
